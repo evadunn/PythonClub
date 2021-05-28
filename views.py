e40d3f0db0c9a2f0ad87799011b244549243b9f5
Eva Dunn <77304836+evadunn@users.noreply.github.com>
2021-05-28 15:58:27 -0700

    second event view commit

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Event, Meetingminutes, Meeting
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def Event(request):
+    Event_list = Event.objects.all()
+    return render(request, 'club/Event.html', {'Event_list': Event_list})
