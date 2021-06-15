53355f75d25463e84bd5cab48af1aeea7e81b2fe
Eva Dunn <77304836+evadunn@users.noreply.github.com>
2021-06-15 11:31:13 -0700

    Authentication test
    Ran 5 tests, 1 failed

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
+import club
 from club.views import MeetingDetail
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Event, Meetingminutes, Meeting, Resource
 import datetime
+from django.urls import reverse_lazy, reverse
 
 # Create your tests here.
 class EventTest(TestCase): 
@@ -27,3 +29,13 @@ class MeetingTest(TestCase):
     def test_meeting(self):
         self.percent = self.meeting
         self.assertEqual(str(Meeting._meta.db_table),'Meeting')
+
+class NEW_ResourceTest_Authentication_Test(TestCase):
+    def setUp(self):
+        self.test_user=User.objects.create_user(username='testuser1', password='p@ssw0rd1')
+        self.type=Resource.objects.create(resourcename='resourcename')
+        self.resource=Resource.objects.create(resourcename='Weekly meeting', resourcetype='Admin', resourceURL='http://www.supplies.com', dateentered=datetime.date(2021, 10, 10), resourcedescription='Supplies')
+
+    def test_redirect_if_not_logged_in(self):
+        response=self.client.get(reverse('newResource.html'))
+        self.assertRedirects(response, '/accounts/login/?next=/club/newResource/.html/')
